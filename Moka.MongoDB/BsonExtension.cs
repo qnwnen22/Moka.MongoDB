09ac0c948bbf8c78fa776420928a898d8f5a90ac
moka <qnwnen0228@gmail.com>
2023-12-27 14:20:30 +0900

    초기 커밋.

---
+++ Moka.MongoDB/BsonExtension.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson;
+
+namespace Moka.MongoDB
+{
+    public static class BsonExtension
+    {
+        public static BsonValue GetBsonValue(this BsonDocument bsonDocument, params string[] keys)
+        {
+            if (bsonDocument == null) return null;
+            BsonDocument result = bsonDocument;
+            BsonValue bsonValue = null;
+            for (int i = 0; i < keys.Length; i++)
+            {
+                string key = keys[i];
+                if (result.Contains(key))
+                {
+                    bsonValue = result[key];
+                }
+                else
+                {
+                    return null;
+                }
+
+                if (i != keys.Length - 1)
+                {
+                    result = bsonValue.AsBsonDocument;
+                }
+            }
+            return bsonValue;
+        }
+
+        public static string GetStringValue(this BsonDocument bsonDocument, params string[] keys)
+        {
+            BsonValue value = GetBsonValue(bsonDocument, keys);
+            if (value == null)
+            {
+                return null;
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+    }
+}
---
+++ Moka.MongoDB/MongoClientExtension.cs
@@ -0,0 +1,68 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Moka.MongoDB
+{
+    public static class MongoClientExtansion
+    {
+        public static string CreateIndex<T>(this IMongoCollection<T> collection, bool unique, params string[] fields)
+        {
+            var indexKeysDefinitionList = new List<IndexKeysDefinition<T>>();
+            for (int i = 0; i < fields.Length; i++)
+            {
+                IndexKeysDefinition<T> item = Builders<T>.IndexKeys.Descending(fields[i]);
+                indexKeysDefinitionList.Add(item);
+            }
+            IndexKeysDefinition<T> combine = Builders<T>.IndexKeys.Combine(indexKeysDefinitionList);
+            
+            var createIndexOptions = new CreateIndexOptions { Unique = unique };
+            
+            CreateIndexModel<T> createIndexModel = new CreateIndexModel<T>(combine, createIndexOptions);
+            return collection.Indexes.CreateOne(createIndexModel);
+        }
+
+        public static string CreateTextIndex<T>(this IMongoCollection<T> collection, params string[] fields)
+        {
+            var indexKeysDefinitionList = new List<IndexKeysDefinition<T>>();
+            for (int i = 0; i < fields.Length; i++)
+            {
+                IndexKeysDefinition<T> item = Builders<T>.IndexKeys.Text(fields[i]);
+                indexKeysDefinitionList.Add(item);
+            }
+            IndexKeysDefinition<T> combine = Builders<T>.IndexKeys.Combine(indexKeysDefinitionList);
+            CreateIndexModel<T> createIndexModel = new CreateIndexModel<T>(combine);
+            return collection.Indexes.CreateOne(createIndexModel);
+        }
+
+        public static bool TryInsert<T>(this IMongoCollection<T> collection, T document)
+        {
+            try
+            {
+                collection.InsertOne(document);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static long TryInsert<T>(this IMongoCollection<T> collection, List<T> documents)
+        {
+            if (documents.Count == 0) return 0;
+            long total = Convert.ToInt64(documents.Count);
+            try
+            {
+                var options = new InsertManyOptions() { IsOrdered = false };
+                collection.InsertMany(documents, options);
+            }
+            catch (MongoBulkWriteException mongoBulkWriteException)
+            {
+                total -= mongoBulkWriteException.WriteErrors.Count;
+            }
+            return total;
+        }
+    }
+}
